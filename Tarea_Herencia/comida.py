919ba4750fdcc7de5c3fdc183a414258de07e3d4
EMILIANO ALEXEI CRUZ MELCHOR <al25760823@ite.edu.mx>
2026-03-21 23:32:11 -0700

    clean repo

--- Tarea_Herencia/comida.py
+++
@@ -1,8 +0,0 @@
-from platillo  import Platillo
-class tipo_platillo(Platillo):
-    def __init__(self, nombre, precio, categoria):
-            super().__init__(nombre, precio)  # Llama al constructor del padre
-            self.categoria = categoria        # Atributo nuevo SOLO de la hija
-
-    def tipo(self):
-            print(f"Tipo: Comida — {self.categoria}")
